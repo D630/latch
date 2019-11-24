56d56610aa2c3ccc8d7783901626cb08fd649703
D630 <d630@posteo.net>
2019-11-24 16:50:33 +0100

    update

--- lib/action/mr.sh
+++ lib/action/mr.sh
@@ -1,75 +1,70 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 mr__main ()
 (
-        if
-                [ "$(idu)" -eq 0 ]
-        then
-                die "may not run as superuser"
-        fi
+	test "$(idu)" -eq 0 &&
+		\die "may not run as superuser";
 
-        bin= \
-        myMrAction= \
-        ytt=;
+	bin=;
+	myMrAction=;
+	ytt=;
 
-        eval set -- "$myArgs"
+	eval set -- "$myArgs";
 
-        if
-                [ "$#" -ne 1 -o "$1" = "{}" ]
-        then
-                die "unknown arguments: '${@}'"
-        else
-                readonly myMrAction="$1"
-        fi
+	if
+		test "$#" -ne 1 -o "$1" = "{}";
+	then
+		\die "unknown arguments: '$*'";
+	else
+		myMrAction=$1;
+		readonly myMrAction;
+	fi
 
-        msg "myMrAction := ${myMrAction}"
+	\msg "myMrAction := $myMrAction";
 
-        cd -- "${myMirror%/*}"
+	cd -- "${myMirror%/*}";
 
-        if
-                ! test -e "$myMirrorList"
-        then
-                skel "mirror" .
-        fi
-        command cp -f -- "$myMirrorList" "${myMirrorList}~"
-        msg "myMirrorList := ${myMirrorList}"
+	test -e "$myMirrorList" ||
+		\skel "mirror" .
+	command cp -f -- "$myMirrorList" "$myMirrorList~";
+	\msg "myMirrorList := $myMirrorList";
 
-        if
-                command -v "mawk" 1>/dev/null 2>&1;
-        then
-                bin="mawk"
-        else
-                bin="awk"
-        fi
+	if
+		command -v mawk 1>/dev/null 2>&1;
+	then
+		bin=mawk;
+	else
+		bin=awk;
+	fi;
 
-        ytt="$(command tty)"
-        readonly ytt
-        export ytt
+	ytt=$(command tty);
+	readonly ytt;
+	export ytt;
 
-        command mkdir -p "${myLog}/mr"
-        command rm -f -- "${myLog}"/mr/?*.log
+	command mkdir -p "$myLog/mr";
+	command rm -f -- "$myLog/mr/"?*.log;
 
-        command "$bin" \
-                -f "${myMirrorList}" \
-                -f "${myRoot}/lib/awk/common.awk" \
-                -f "${myRoot}/lib/awk/mr.awk" \
-                -- -a "$myMrAction" \
-        | command xargs -E '' -L 1 -P 4 -x -r sh -c '
-                set -e;
-                exec >> "${myLog}/mr/${$}.log";
-                exec 2>&1;
-                echo "latch/mr: processing ${1} ...";
-                . "${myRoot}/lib/setup.sh";
-                import mr;
-                #WORKTREE="${myCheckout}/${1%.git}";
-                MIRROR="${myMirror}/${1}";
-                command mkdir -p "$MIRROR" #"$WORKTREE";
-                cd -- "$MIRROR";
-                export MIRROR #WORKTREE;
-                eval "$2";
-                # TODO
-                command cat "${myLog}/mr/${$}.log" > "$ytt";
-        ' sh;
+	command "$bin" \
+		-f "$myMirrorList" \
+		-f "$myRoot/lib/awk/common.awk" \
+		-f "$myRoot/lib/awk/mr.awk" \
+		-- -a "$myMrAction" \
+	| command xargs -E '' -L 1 -P 4 -x -r sh -c '
+		set -e;
+		exec >> "${myLog}/mr/${$}.log";
+		exec 2>&1;
+		echo "latch/mr: processing $1 ...";
+		. "$myRoot/lib/setup.sh";
+		import mr;
+		# WORKTREE=$myCheckout/${1%.git};
+		MIRROR=$myMirror/$1;
+		command mkdir -p "$MIRROR" # "$WORKTREE";
+		cd -- "$MIRROR";
+		export MIRROR # WORKTREE;
+		eval "$2";
+		# TODO
+		command cat "$myLog/mr/$$.log" > "$ytt";
+	' sh;
 )
 
-# vim: set ts=8 sw=8 tw=0 et :
+# vim: set ft=sh :
