09480202654f88e70469458993c6c83aabcb6792
D630 <d630@posteo.net>
2019-11-24 14:41:59 +0100

    update

--- lib/stow/stow.sh
+++ lib/stow/stow.sh
@@ -1,55 +1,50 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 slimit ()
 {
-        local _l
-        _l=_
+	local _l;
+	_l=_;
 
-        case "${isInitialized}::${isPacked}::${isStowed}" in
-        false::*)
-                :
-        ;;
-        true::false::*)
-                :
-        ;;
-        true::true::false)
-                if
-                        [ "$stowedIs" = "null" ]
-                then
-                        _l="add"
-                else
-                        _l="delete"
-                fi
-        ;;
-        true::true::true)
-                _l="delete"
-        esac
+	case $isInitialized::$isPacked::$isStowed in
+		(false::*|true::false::*)
+			: ;;
+		true::true::false)
+			if
+				test "$stowedIs" = null;
+			then
+				_l=add;
+			else
+				_l=delete;
+			fi;;
+		(true::true::true)
+			_l=delete;
+	esac;
 
-        if
-                [ "$_l" = "$myStowAction" ]
-        then
-                msg "{ ${_l} }"
-        else
-                die "myStowAction cannot be executed: '${myStowAction}'"
-        fi
+	if
+		test "$_l" = "$myStowAction";
+	then
+		\msg "{ $_l }";
+	else
+		\die "myStowAction cannot be executed: '$myStowAction'";
+	fi;
 }
 
 sstow ()
 {
-        command xstow -v 3 \
-                -F "$myXstowConfig" \
-                -dir "$STOW_DIR" \
-                -target "$STOW_TARGET" \
-                "${1:-$PKG_NAME}";
+	command xstow -v 3 \
+		-F "$myXstowConfig" \
+		-dir "$STOW_DIR" \
+		-target "$STOW_TARGET" \
+		"${1:-$PKG_NAME}";
 }
 
 sunstow ()
 {
-        command xstow -v 3 \
-                -F "$myXstowConfig" \
-                -dir "$STOW_DIR" \
-                -target "$STOW_TARGET" \
-                -D "${1:-$PKG_NAME}";
+	command xstow -v 3 \
+		-F "$myXstowConfig" \
+		-dir "$STOW_DIR" \
+		-target "$STOW_TARGET" \
+		-D "${1:-$PKG_NAME}";
 }
 
-# vim: set ts=8 sw=8 tw=0 et :
+# vim: set ft=sh :
