ac3a72da432854736069c09d04dfe8404bdb3f20
D630 <d630@posteo.net>
2019-11-24 14:47:25 +0100

    update

--- lib/pkg/pkg.sh
+++ lib/pkg/pkg.sh
@@ -1,67 +1,55 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 __plimit ()
 {
-        if
-                [ "$arePacked" -gt 1 ]
-        then
-                _l="${_l} chop"
-        else
-                :
-        fi
+	test "$arePacked" -gt 1 &&
+		_l="$_l chop";
 }
 
-_plimit ()
-if
-        ! [ "$stowedIs" = "null" ]
-then
-        case "$myPkgAction" in
-        purge)
-                die "current stowed version must be unstowed: '${stowedIs}'"
-        esac
-        __plimit
-else
-        :
-fi
+_plimit () {
+	test "$stowedIs" = "null" || {
+		case $myPkgAction in
+			(purge)
+				\die "current stowed version must be unstowed: '$stowedIs'";;
+		esac;
+		\__plimit;
+	};
+}
 
 plimit ()
 {
-        local _l
+	local _l
 
-        case "${isInitialized}::${isPacked}::${isStowed}" in
-        false::*)
-                _l="init build"
-        ;;
-        true::false::*)
-                _l="purge build install"
-                _plimit
-        ;;
-        true::true::false)
-                _l="purge build remove"
-                _plimit
-        ;;
-        true::true::true)
-                _l="build"
-                __plimit
-        esac
+	case $isInitialized::$isPacked::$isStowed in
+		(false::*)
+			_l="init build";;
+		(true::false::*)
+			_l="purge build install";
+			\_plimit;;
+		(true::true::false)
+			_l="purge build remove";
+			\_plimit;;
+		(true::true::true)
+			_l=build;
+			\__plimit;;
+	esac;
 
-        if
-            test "${_l:-_}" = "_"
-        then
-            die "alarm. something went wrong, really"
-        else
-            _l=" $_l "
-        fi
+	if
+		test "${_l:-_}" = _;
+	then
+		\die "alarm. something went wrong, really";
+	else
+		_l=" $_l ";
+	fi;
 
-        msg "{${_l}}"
+	\msg "{${_l}}";
 
-        case "$_l" in
-        *" ${myPkgAction} "*)
-                :
-        ;;
-        *)
-                die "myPkgAction cannot be executed: '${myPkgAction}'"
-        esac
+	case $_l in
+		(*" $myPkgAction "*)
+			:;;
+		(*)
+			\die "myPkgAction cannot be executed: '$myPkgAction'";
+	esac;
 }
 
-# vim: set ts=8 sw=8 tw=0 et :
+# vim: set ft=sh :
